fee8b788a077d3c46afc2e0b49999daa96665a39
niklasv <niklasv@2d33525a-e5fd-0310-a46b-adbed02b8dd7>
2005-08-12 07:23:56 +0000

    - Added license header; added method get_plugin(key)

--- trunk/Sloppy/src/Sloppy/application.py
+++ trunk/Sloppy/src/Sloppy/application.py
@@ -1,3 +1,23 @@
+# This file is part of SloppyPlot, a scientific plotting tool.
+# Copyright (C) 2005 Niklas Volbers
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+# $HeadURL$
+# $Id$
+
 import logging
 logging.basicConfig(level=logging.INFO)
 
@@ -139,6 +159,11 @@ class Application(object):
         for key in PluginRegistry.iterkeys():
             self.plugins[key] = PluginRegistry.new_instance(key, app=self)
 
+    def get_plugin(self, key):
+        try:
+            return self.plugins[key]
+        except KeyError:
+            raise RuntimeError("Requested plugin '%s' is not available." % key)
 
 
 #------------------------------------------------------------------------------
