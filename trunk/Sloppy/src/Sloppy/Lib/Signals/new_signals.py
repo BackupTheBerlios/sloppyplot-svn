075b286c222cbaf357a0ba7c6602a035b8bd472b
niklasv <niklasv@2d33525a-e5fd-0310-a46b-adbed02b8dd7>
2005-11-03 11:36:42 +0000

    sync.

--- trunk/Sloppy/src/Sloppy/Lib/Signals/new_signals.py
+++ trunk/Sloppy/src/Sloppy/Lib/Signals/new_signals.py
@@ -23,89 +23,120 @@
 
 # register signal => new entry in self._signals
 # connect signal => new entry in self._slots
-# maybe write a class CallbackManager, that has signals and slots
+# maybe write a class HasSignals, that has signals and slots
 # and has methods 'connect', 'disconnect' and so on.  It would
-# have to know the sender, of course.
+# _be_ the sender.
+
+
+# class Signal:
+
+#     def __init__(self, name, callback, *args, **kwargs):
+#         logger.debug("new signal...")
+
+#         self.name = name
+
+#         # TODO: use inspect for this
+        
+#         # Signal.receiver is a weak reference to the receiving object.
+#         # The user should not explicitly provide this object, so
+#         # we need to extract this information from the given callback.
+#         # We perform this separation of object and unbound method via
+#         # the methods im_self and im_func. If `callback` is only
+#         # a method, not a method bound to an object, then an
+#         # AttributeError is raised, caught and the receiver is set to
+#         # the so called Anonymous receiver.
+#         try:
+#             # Try bound method.
+#             # If unbound, an AttributeError will occur
+#             receiver = callback.im_self
+#             callback = callback.im_func
+#             logger.debug("(bound to object)")
+#         except AttributeError:
+#             # unbound method
+#             receiver = Anonymous
+#             callback = callback
+#             logger.debug("(unbound)")
+
+#         self.callback = callback
+#         self.receiver = weakref.ref(receiver)
+        
+#         self.args = args
+#         self.kwargs = kwargs
+
+
+
+
+#     def connect_signal(self, signal_name, callback, *args, **kwargs):
+#         new_signal = Signal(signal_name, callback, *args, **kwargs)
+#         self._signals.append(new_signal)
+#         return newSignal
+
+#     def disconnect_signal(self, signal=None, signal_name=None, receiver=None):
+#         """ Disconnect all signals matching the given criteria. """
+#         if signal is not None:
+#             signals = [signal]
+#         else:
+#             signals = self.get_signals(receiver=receiver, signal_name=signal_name)
+
+#         for signal in signals:
+#             try:
+#                 self._signals.remove(signal)
+#             except ValueError:
+#                 logger.error("disconnect_signal: signal not found!")
+
+#     def disconnect_signals(self, signal_list):
+#         """ Disconnect all given signals. """
+#         while len(signals) > 0:
+#             self.disconnect_signal(signal_list.pop(0))
+
+#     def emit_signal(self, signal_name, *args, **kwargs):
+#         pass
+
+#     def get_signals(self, receiver=None, signal_name=None):
+#         """ Return all signals that match the given criteria. """
+#         signals = self.signals
+#         if receiver is not None:
+#             signals = [signal for signal in signals if id(signal.receiver()) == id(receiver)]
+#         if signal_name is not None:
+#             signals = [signal for signal in signals if id(signal.name) == id(signal_name)]
+#         return signals                       
 
 
 class Signal:
-
-    def __init__(self, sender, name, callback, *args, **kwargs):
-        logger.debug("new signal...")
-
-        self.sender = sender
-        self.name = name
-
-        # Signal.receiver is a weak reference to the receiving object.
-        # The user should not explicitly provide this object, so
-        # we need to extract this information from the given callback.
-        # We perform this separation of object and unbound method via
-        # the methods im_self and im_func. If `callback` is only
-        # a method, not a method bound to an object, then an
-        # AttributeError is raised, caught and the receiver is set to
-        # the so called Anonymous receiver.
-        try:
-            # Try bound method.
-            # If unbound, an AttributeError will occur
-            receiver = callback.im_self
-            callback = callback.im_func
-            logger.debug("(bound to object)")
-        except AttributeError:
-            # unbound method
-            receiver = Anonymous
-            callback = callback
-            logger.debug("(unbound)")
-
-        self.callback = callback
-        self.receiver = weakref.ref(receiver)
-        
+    def __init__(self, *args, **kwargs):
         self.args = args
         self.kwargs = kwargs
 
-
-    
+        
 class HasSignals:
 
     def __init__(self):
-        self._signals = [] # list of Signal objects
-        self._slots = {} # 
+        self._signals = {} # dictionary of Signal objects (keys: signal names)
+        self._callbacks = {} # dictionary of callbacks (keys: signal names)
 
+    def register_signal(self, signal_name):   
+        self._signals[signal_name] = Signal()
+        self._slots[signal_name] = []
 
-    def connect_signal(self, signal_name, callback, *args, **kwargs):
-        new_signal = Signal(sender, signal_name, callback, *args, **kwargs)
-        self._signals.append(new_signal)
-        return newSignal
+    def connect_signal(self, signal_name, callback):
+        pass
+                                       
 
-    def disconnect_signal(self, signal=None, signal_name=None, receiver=None):
-        """ Disconnect all signals matching the given criteria. """
-        if signal is not None:
-            signals = [signal]
-        else:
-            signals = self.get_signals(receiver=receiver, signal_name=signal_name)
-
-        for signal in signals:
-            try:
-                self._signals.remove(signal)
-            except ValueError:
-                logger.error("disconnect_signal: signal not found!")
-
-    def disconnect_signals(self, signal_list):
-        """ Disconnect all given signals. """
-        while len(signals) > 0:
-            self.disconnect_signal(signal_list.pop(0))
-
-    def emit_signal(self, signal_name, *args, **kwargs):
+def test():
+    class TestClass(HasSignals):
         pass
 
-    def get_signals(self, receiver=None, signal_name=None):
-        """ Return all signals that match the given criteria. """
-        signals = self.signals
-        if receiver is not None:
-            signals = [signal for signal in signals if id(signal.receiver()) == id(receiver)]
-        if signal_name is not None:
-            signals = [signal for signal in signals if id(signal.name) == id(signal_name)]
-        return signals                       
+    def receiver(self, sender, value):
+        print "Signal with value %s" % value
         
-                                       
-                                       
+        
+    tc = TestClass()
+    tc.sig_register("notify")
+
+    tc.sig_connect("notify", receiver)
+    tc.sig_emit("notify")
+    
+    
+if __name__ == "__main__":
+    test()
     
