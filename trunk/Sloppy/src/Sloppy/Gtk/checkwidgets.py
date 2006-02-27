c599fd13f68825d6793f9f4440d14ea7da050c6e
niklasv <niklasv@2d33525a-e5fd-0310-a46b-adbed02b8dd7>
2006-02-27 12:53:57 +0000

    more work on checkwidgets

--- trunk/Sloppy/src/Sloppy/Gtk/checkwidgets.py
+++ trunk/Sloppy/src/Sloppy/Gtk/checkwidgets.py
@@ -1,6 +1,5 @@
 
-import gtk
-
+import gtk, sys
 
 from Sloppy.Lib.Check import *
 from Sloppy.Lib.Undo import UndoList
@@ -8,16 +7,19 @@ from Sloppy.Base import uwrap
 
 import logging
 logger = logging.getLogger('gtk.checkwidgets')
-#------------------------------------------------------------------------------
 
+
+#
 # TODO:
+#
+#   Display_Mapping_As_Combobox (!)
+#   Display_RGBColor (!!!)
+#   Display_Bool_As_Checkbutton => boring
+#
 
-# Display_Mapping_As_Combobox (!)
-# Display_RGBColor (!!!)
-# Display_Number_As_SpinButton
-# Display_Bool_As_Checkbutton => boring
 
-#------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+
 
 class Display:
     
@@ -27,204 +29,190 @@ class Display:
         self.check = None
         self.set_source(obj, key)
 
-        self.widget = None
         self.last_value = None
-        self.init()
+        self.widget = None
 
-        self.create_widget()
-        self.prepare_widget()
+        self.widget = self.create_widget()
+        self.prepare_widget(self.widget)
 
     def set_source(self, obj, key):        
         self.obj = obj
         self.key = key
         self.check = obj._checks[key]
-                
-    def init(self):
-        pass
 
-   
+    #----------------------------------------------------------------------
+    # UI Stuff
+
+    def use_widget(self, widget):
+        self.widget = widget
+        self.prepare_widget(widget)
+                
     #----------------------------------------------------------------------
     # Check In/Out
     
     def check_in(self):
         " Retrieve value from object. "
-        raise RuntimeError("check_in() needs to be implemented.")
+        value = self.get_object_data()
+        self.set_widget_data(value)
+        self.last_value = value
 
-    def widget_data(self):
-        """ Data that can be passed on to the object. """
-        raise RuntimeError("widget_data() needs to be implemented.")
-    
     def check_out(self, undolist=[]):
         " Set value in object. "
-        new_value = self.widget_data()
-        if new_value != self.last_value:
-            uwrap.smart_set(self.obj, self.key, new_value, undolist=undolist)
-            self.last_value = new_value
+        new_value = self.get_widget_data()
+        uwrap.smart_set(self.obj, self.key, new_value, undolist=undolist)
 
-    #----------------------------------------------------------------------
-    # UI Stuff
+    def get_object_data(self):
+        return self.obj.get(self.key)
 
-    def create_widget(self):
-        raise RuntimeError("create_widget() needs to be implemented.")
 
-    def use_widget(self, widget):
-        self.widget = widget
-        self.prepare_widget()
-        
-
-class Display_Bool_As_Combobox(Display):
-
-    def init(self):
-        self.last_index = None
-
-    def create_widget(self):
-        cb = gtk.ComboBox()
-        model = gtk.ListStore(str, object)
-        cb.set_model(model)
-        cell = gtk.CellRendererText()
-        cb.pack_start(cell, True)
-        cb.add_attribute(cell, 'text', 0)
-        self.widget = cb
-        return cb
     
-    def prepare_widget(self):
-        adict = {'True': True, 'False': False}
-        if self.obj._checks[self.key].required is False:
-            adict.update({'None': None})
-        self.values = adict.values() # for reference
 
-        model = self.widget.get_model()
-        model.clear()        
-        for key, value in adict.iteritems():
-            model.append((key, value))            
-        
-    def check_in(self):
-        value = self.obj.get(self.key)
-        if value is not Undefined:
-            index = self.values.index(value)
-            model = self.widget.get_model()
-            iter = model.get_iter((index,))
-            self.widget.set_active_iter(iter)
+
+class As_Combobox:
+    # self.values
+    # model: str, object
+
+    def create_widget(self):
+        return gtk.ComboBox()
+    
+    def get_widget_data(self):
+        index = self.widget.get_active()
+        if index < 0:
+            return Undefined            
         else:
+            model = self.widget.get_model()
+            return model[index][1]
+
+    def set_widget_data(self, data):
+        try:
+            index = self.values.index(data)
+        except:
             index = -1
-            
-        self.last_value = value
-        self.last_index = index
-
-    def widget_data(self):
-        index = self.widget.get_active()
-        if index == self.last_index:
-            return self.last_value
-        elif index < 0:
-            return Undefined            
-        else:
-            model = self.widget.get_model()
-            return model[index][1]
-
-    def check_out(self, undolist=[]):
-        new_value = self.widget_data()
-        if new_value != self.last_value:
-            uwrap.smart_set(self.obj, self.key, new_value, undolist=undolist)
-            self.last_value = new_value
+        self.widget.set_active(index)
 
 
-class Display_Choice_As_Combobox(Display):
-
-    def init(self):
-        self.last_index = -1
-
-    def create_widget(self):
-        self.widget = gtk.ComboBox()
-        return self.widget
-        
-    def prepare_widget(self):
-        cb = self.widget
-        model = gtk.ListStore(str, object)
-        cb.set_model(model)
-        cell = gtk.CellRendererText()
-        cb.pack_start(cell, True)
-        cb.add_attribute(cell, 'text', 0)
-
-        model.clear()
-        for value in self.check.choices:
-            model.append((unicode(value), value))
-
-    def check_in(self):
-        value = self.obj.get(self.key)
-        if value != Undefined:
-            index = self.check.choices.index(value)
-            model = self.widget.get_model()
-            iter = model.get_iter((index,))
-            self.widget.set_active_iter(iter)
-            self.last_index = index
-            
-        self.last_value = value
-
-    
-    def widget_data(self):
-        index = self.widget.get_active()
-        if index == self.last_index:
-            return self.obj.get(self.key)
-        elif index < 0:
-            return Undefined            
-        else:
-            model = self.widget.get_model()
-            return model[index][1]
-
-
-class Display_Anything_As_Entry(Display):
+class As_Entry:
 
     def create_widget(self):                      
-        self.widget = gtk.Entry()
-        return self.widget
+        return gtk.Entry()
 
-    def prepare_widget(self):
-        entry = self.widget        
-        entry.connect("focus-in-event", self.on_focus_in_event)
-        entry.connect("focus-out-event", self.on_focus_out_event)
-
-    def on_focus_in_event(self, widget, event):
-        self.last_value = widget.get_text()
-        
-    def on_focus_out_event(self, widget, event):
-        value = widget.get_text()
-        if value == self.last_value:
-            return
-
-        if self.check.required is False and value == "":
-            return
-            
-        try:
-            value = self.check(value)
-            widget.set_text(unicode(value))
-        except ValueError:
-            widget.set_text(self.last_value)
-                            
-        return False
-            
-
-    def check_in(self):
-        value = self.obj.get(self.key)
+    def get_widget_data(self):
+        value = self.widget.get_text()
         if value is Undefined:
-            value = None
-            
-        if value is None:
             value = ""
 
-        self.widget.set_text(unicode(value))
-        self.last_value = value
-
-
-    def widget_data(self):
-        value = self.widget.get_text()
         if self.check.required is False and value=="":
             return None
             
         try:
             return self.check(value)
-        except Exception, msg:
-            return self.last_value
+        except ValueError:
+            self.set_widget_data(self.last_value)
 
+    def set_widget_data(self, data):
+        if data is Undefined or data is None:
+            data = u""
+        else:
+            data = unicode(data)
+            
+        self.widget.set_text(data)
+
+    
+
+class Display_Bool_As_Combobox(As_Combobox, Display):
+            
+    def prepare_widget(self, combobox):
+        model = gtk.ListStore(str, object)
+        combobox.set_model(model)
+        cell = gtk.CellRendererText()
+        combobox.pack_start(cell, True)
+        combobox.add_attribute(cell, 'text', 0)
+        
+        adict = {'True': True, 'False': False}
+        if self.obj._checks[self.key].required is False:
+            adict.update({'None': None})
+        self.values = adict.values() # for reference
+
+        model = combobox.get_model()
+        model.clear()        
+        for key, value in adict.iteritems():
+            model.append((key, value))            
+        
+        
+
+class Display_Choice_As_Combobox(As_Combobox, Display):
+        
+    def prepare_widget(self, cb):
+        model = gtk.ListStore(str, object)
+        cb.set_model(model)
+        cell = gtk.CellRendererText()
+        cb.pack_start(cell, True)
+        cb.add_attribute(cell, 'text', 0)
+        model.clear()
+        for value in self.check.choices:
+            model.append((unicode(value), value))
+        
+
+class Display_Anything_As_Entry(As_Entry, Display):
+
+    def prepare_widget(self, entry):
+        entry.connect("focus-in-event", self.on_focus_in_event)
+
+    def on_focus_in_event(self, widget, event):
+        self.widget.connect("focus-out-event", self.on_focus_out_event)
+        
+    def on_focus_out_event(self, widget, event):
+        value = widget.get_text()
+        if self.check.required is False and value == "":
+            return
+
+        try:
+            value = unicode(self.check(value))
+            self.last_value = value
+            self.set_widget_data(value)
+        except ValueError:
+            self.set_widget_data(self.last_value)
+
+        return False    
+                
+
+
+class As_Spinbutton:
+
+    def create_widget(self):
+        return gtk.SpinButton()
+
+    def get_widget_data(self):
+        return self.widget.get_value()
+    
+    def set_widget_data(self, data):
+        if data not in (None, Undefined):
+            self.widget.set_value(float(data))
+        
+class Display_Number_As_Spinbutton(As_Spinbutton, Display):
+    
+    def prepare_widget(self, spinbutton):
+        check = self.obj._checks[self.key]
+
+        spinbutton.set_numeric(False)
+
+        # should be customizable        
+        spinbutton.set_increments(1, 1)
+        spinbutton.set_digits(2) 
+
+        min, max = check.min, check.max
+        if min is None:
+            min = -sys.maxint
+        if max is None:
+            max = +sys.maxint            
+        spinbutton.set_range(min, max)
+
+class Display_Integer_As_Spinbutton(Display_Number_As_Spinbutton):
+
+    def prepare_widget(self, spinbutton):
+        Display_Number_As_Spinbutton.prepare_widget(self, spinbutton)
+        spinbutton.set_digits(0)
+        
 
 Display_Number_As_Entry = Display_Anything_As_Entry
 Display_String_As_Entry = Display_Unicode_As_Entry = Display_Anything_As_Entry
@@ -240,6 +228,7 @@ class TestObject(HasChecks):
     a_float = Float()
     another_float = Float(max=27.0)
     a_third_float = Float(min=-5, max=12.874)
+    what_an_integer = Integer(max=20)
     
 
 obj = TestObject(is_valid=False)
@@ -250,7 +239,9 @@ cdict = {'is_valid': Display_Bool_As_Combobox,
          'an_integer': Display_Number_As_Entry,
          'a_float': Display_Number_As_Entry,
          'another_float': Display_Number_As_Entry,
-         'a_third_float': Display_Number_As_Entry}
+         'a_third_float': Display_Number_As_Spinbutton,
+         'what_an_integer': Display_Integer_As_Spinbutton}
+
 clist = []
 for key, connector in cdict.iteritems():
     new_connector = connector(obj, key)
@@ -271,7 +262,11 @@ win.connect('destroy', quit)
 
 vbox = gtk.VBox()
 for c in clist:
-    vbox.pack_start(c.widget, False, True)
+    hbox = gtk.HBox()
+    label = gtk.Label(c.key)
+    hbox.pack_start(label, False, False)
+    hbox.pack_start(c.widget, True, True)
+    vbox.pack_start(hbox, False, True)
 
 win.add(vbox)
 win.show_all()
