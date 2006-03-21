0c57bf268e55ea17c94f4d64bf6e30b755e6955a
niklasv <niklasv@2d33525a-e5fd-0310-a46b-adbed02b8dd7>
2006-03-21 07:42:19 +0000

    sync.

--- trunk/Sloppy/src/Sloppy/Matplot/mpl2.py
+++ trunk/Sloppy/src/Sloppy/Matplot/mpl2.py
@@ -78,75 +78,6 @@ linemarker_mappings = \
 
 #------------------------------------------------------------------------------
 
-class Backend(backend.Backend):
-
-    active_layer = Instance(objects.Layer, init=None)
-    
-    def init(self):
-        self.painters = {} # == layers
-        self.sig_register('redraw')
-        self.sig_connect('redraw', lambda sender: self.redraw())
-
-    def connect(self):
-        self.figure = Figure(dpi=100, facecolor="white")
-        self.canvas = FigureCanvas(self.figure)
-        backend.Backend.connect(self)
-
-    def disconnect(self):
-        if self.canvas is not None:
-            self.canvas.destroy()
-            self.canvas = None
-        if self.figure is not None:
-            self.figure = None
-        backend.Backend.disconnect(self)
-
-    def get_painter(self, obj, klass=None):
-        _id = id(obj)
-        try:
-            return self.painters[_id]
-        except KeyError, msg:
-            if klass is not None:
-                new_painter = klass(obj, parent=self)
-                self.painters[_id] = new_painter
-                return new_painter
-            else:
-                raise       
-
-    def draw(self):
-        for layer in self.plot.layers:
-            painter = self.get_painter(layer, LayerPainter)
-            painter.paint()
-        self.canvas.draw()
-        self._redraw = False        
-
-    def on_update_layers(self, sender, updateinfo):
-        # TODO!!!!
-        # check if active layer is still active
-        removed = updateinfo.get('removed', [])
-        if self.active_layer in removed:
-            self.set_active_layer(None)
-
-    def request_active_layer(self):
-        """ Return the active layer or if it is None, try to set it first. """
-        if self.active_layer is None:
-            if len(self.plot.layers) > 0:
-                self.active_layer = self.plot.layers[0]
-        return self.active_layer
-
-
-    def queue_redraw(self):
-        self._redraw = True
-
-    def redraw(self, force=False):
-        """ redraw, unlike draw, only redisplays the existing canvas. """
-        if self._redraw or force is True:
-            print "REDRAW"
-            self.canvas.draw()
-        self._redraw = False
-        
-
-#------------------------------------------------------------------------------
-
 class Painter(SPObject):
 
     def __init__(self, obj, parent):
@@ -180,111 +111,53 @@ class Painter(SPObject):
         while not isinstance(obj, Backend):
             obj = obj.parent
         return obj
-    
-        
-class LayerPainter(Painter):
-
-    active_line = Instance(objects.Line, init=None)
-    line_cache = Dict(keys=Instance(Line))
-    
-    def init(self):
-        self.axes = self.init_axes()
-        self.obj.sig_connect('update', self.update)
-        self.obj.sig_connect('update::title', self.update_title)
-
-        # updating the axis with start/end = None does not work
-        # right now. Somehow the axis is not redrawn at all.
-        # Therefore this is a work-around: we simply redraw the
-        # complete layer!
-
-        # TODO: can the axis change?
-        self.obj.xaxis.sig_connect('update', lambda sender, keys: self.paint())
-        self.obj.yaxis.sig_connect('update', lambda sender, keys: self.paint())
+                
         
 
-    def init_axes(self):
-        return self.parent.figure.add_subplot('111')
 
+class LegendPainter(Painter):
 
-    def update_title(self, sender, new_title):
-        # title
-        title = new_title
-        self.axes.set_title(title or '')
+    def paint(self):
+        legend, layer_painter = self.obj, self.parent
 
+        kwargs = self.get_kwargs()
+        border = kwargs.pop('border')
+        visible = kwargs.pop('visible')
+        handles, labels = kwargs.pop('handles'), kwargs.pop('labels')
+        print "labels", labels
+        _legend = layer_painter.axes.legend(handles, labels, **kwargs)        
+        _legend.draw_frame(border)
+        _legend.set_visible(visible)
+                
 
-    def update(self, sender, keys):
+    def get_kwargs(self):
+        legend, layer_painter = self.obj, self.parent
 
-        # plot_painter := backend
-        layer, plot_painter = self.obj, self.parent
-        axes = self.axes
-        
-        # title
-        if 'title' in keys:
-            title = layer.title
-            axes.set_title(title or '') # matplotlib doesn't like None as title
+        # visible
+        visible = legend.visible
+                    
+        # labels # TODO
+        label = legend.label
+        if label is not None:
+            pass
 
-        # grid
-        if 'grid' in keys:
-            axes.grid(layer.grid)
+        # border
+        border = legend.border
 
-        
-        # lines -- these should be updated by update::lines !!!
-        for line in layer.lines:
-            painter = self.get_painter(line, LinePainter)
-            painter.paint()
+        # position
+        position = legend.position
+        if position == 'at position':
+            position = (legend.x, legend.y)
 
-        # axes
-        # This needs to be after lines, because painting the
-        # lines would reset the start and end.
-        if 'xaxis' in keys or 'yaxis' in keys:
-            self.update_axis(None, [])
+        # create legend entries from line labels
+        line_cache = layer_painter.axes.lines
+        labels = [l.get_label() for l in line_cache]
 
-        # legend
-        # Since the legend labels are constructed from the matplotlib
-        # line objects, it is necessary to construct the legend after
-        # the lines are plotted
-        if 'legend' in keys:
-            legend = layer.legend        
-            if legend is None:
-                # remove any existing legend painter if obsolete
-                pass
-            else:
-                p = self.get_painter(legend, LegendPainter)
-                p.paint()
-            
-        self.get_backend().queue_redraw()
-
-    def update_axis(self, sender, keys):
-        
-        layer = self.obj
-        axes = self.axes
-        for (key, axis) in layer.axes.iteritems():
-            print "SETTING RANGE"
-            #:axis.label, :axis.scale, :axis.start,:axis.end
-            label, scale, start, end = axis.label, axis.scale, axis.start, axis.end
-            logger.debug("::: start = %s; end = %s" % (start, end))
-
-            if key == 'x':
-                set_label = axes.set_xlabel
-                set_scale = axes.set_xscale
-                set_start = (lambda l: axes.set_xlim(xmin=l))
-                set_end = (lambda l: axes.set_xlim(xmax=l))
-            elif key == 'y':
-                set_label = axes.set_ylabel
-                set_scale = axes.set_yscale
-                set_start = (lambda l: axes.set_ylim(ymin=l))
-                set_end = (lambda l: axes.set_ylim(ymax=l))
-            else:
-                raise RuntimeError("Invalid axis key '%s'" % key)
-
-            if label is not None: set_label(label)
-            if scale is not None: set_scale(scale)
-
-            set_start(start)
-            set_end(end)
-
-        self.get_backend().queue_redraw()
-            
+        return  {'handles' : line_cache,
+                 'labels' : labels,
+                 'loc' : position,
+                 'border' : border,
+                 'visible' : visible}
 
 class LinePainter(Painter):
 
@@ -396,53 +269,182 @@ class LinePainter(Painter):
                 label = line.label
         return label
 
+
+
+
+class LayerPainter(Painter):
+
+    active_line = Instance(objects.Line, init=None)
+    line_cache = Dict(keys=Instance(Line))
+    
+    def init(self):
+        self.axes = self.init_axes()
+        self.obj.sig_connect('update', self.update)
+        self.obj.sig_connect('update::title', self.update_title)
+
+        # updating the axis with start/end = None does not work
+        # right now. Somehow the axis is not redrawn at all.
+        # Therefore this is a work-around: we simply redraw the
+        # complete layer!
+
+        # TODO: can the axis change?
+        self.obj.xaxis.sig_connect('update', lambda sender, keys: self.paint())
+        self.obj.yaxis.sig_connect('update', lambda sender, keys: self.paint())
         
+
+    def init_axes(self):
+        return self.parent.figure.add_subplot('111')
+
+
+    def update_title(self, sender, new_title):
+        # title
+        title = new_title
+        self.axes.set_title(title or '')
+
+
+    def update(self, sender, keys):
+
+        # plot_painter := backend
+        layer, plot_painter = self.obj, self.parent
+        axes = self.axes
         
+        # title
+        if 'title' in keys:
+            title = layer.title
+            axes.set_title(title or '') # matplotlib doesn't like None as title
+
+        # grid
+        if 'grid' in keys:
+            axes.grid(layer.grid)
+
+        
+        # lines -- these should be updated by update::lines !!!
+        for line in layer.lines:
+            painter = self.get_painter(line, LinePainter)
+            painter.paint()
+
+        # axes
+        # This needs to be after lines, because painting the
+        # lines would reset the start and end.
+        if 'xaxis' in keys or 'yaxis' in keys:
+            self.update_axis(None, [])
+
+        # legend
+        # Since the legend labels are constructed from the matplotlib
+        # line objects, it is necessary to construct the legend after
+        # the lines are plotted
+        if 'legend' in keys:
+            legend = layer.legend        
+            if legend is None:
+                # remove any existing legend painter if obsolete
+                pass
+            else:
+                p = self.get_painter(legend, LegendPainter)
+                p.paint()
+            
+        self.get_backend().queue_redraw()
+
+    def update_axis(self, sender, keys):
+        
+        layer = self.obj
+        axes = self.axes
+        for (key, axis) in layer.axes.iteritems():
+            print "SETTING RANGE"
+            #:axis.label, :axis.scale, :axis.start,:axis.end
+            label, scale, start, end = axis.label, axis.scale, axis.start, axis.end
+            logger.debug("::: start = %s; end = %s" % (start, end))
+
+            if key == 'x':
+                set_label = axes.set_xlabel
+                set_scale = axes.set_xscale
+                set_start = (lambda l: axes.set_xlim(xmin=l))
+                set_end = (lambda l: axes.set_xlim(xmax=l))
+            elif key == 'y':
+                set_label = axes.set_ylabel
+                set_scale = axes.set_yscale
+                set_start = (lambda l: axes.set_ylim(ymin=l))
+                set_end = (lambda l: axes.set_ylim(ymax=l))
+            else:
+                raise RuntimeError("Invalid axis key '%s'" % key)
+
+            if label is not None: set_label(label)
+            if scale is not None: set_scale(scale)
+
+            set_start(start)
+            set_end(end)
+
+        self.get_backend().queue_redraw()
+            
+
+#------------------------------------------------------------------------------
+
+class Backend(backend.Backend):
+
+    active_layer = Instance(objects.Layer, init=None)
+    
+    def init(self):
+        self.painters = {} # == layers
+        self.sig_register('redraw')
+        self.sig_connect('redraw', lambda sender: self.redraw())
+
+    def connect(self):
+        self.figure = Figure(dpi=100, facecolor="white")
+        self.canvas = FigureCanvas(self.figure)
+        backend.Backend.connect(self)
+
+    def disconnect(self):
+        if self.canvas is not None:
+            self.canvas.destroy()
+            self.canvas = None
+        if self.figure is not None:
+            self.figure = None
+        backend.Backend.disconnect(self)
+
+    def get_painter(self, obj, klass=None):
+        _id = id(obj)
+        try:
+            return self.painters[_id]
+        except KeyError, msg:
+            if klass is not None:
+                new_painter = klass(obj, parent=self)
+                self.painters[_id] = new_painter
+                return new_painter
+            else:
+                raise       
+
+    def draw(self):
+        for layer in self.plot.layers:
+            painter = self.get_painter(layer, LayerPainter)
+            painter.paint()
+        self.canvas.draw()
+        self._redraw = False        
+
+    def on_update_layers(self, sender, updateinfo):
+        # TODO!!!!
+        # check if active layer is still active
+        removed = updateinfo.get('removed', [])
+
+        #if self.active_layer in removed:
+        #    self.set_active_layer(None)
+
+    def request_active_layer(self):
+        """ Return the active layer or if it is None, try to set it first. """
+        if self.active_layer is None:
+            if len(self.plot.layers) > 0:
+                self.active_layer = self.plot.layers[0]
+        return self.active_layer.obj
 
 
-class LegendPainter(Painter):
+    def queue_redraw(self):
+        self._redraw = True
 
-    def paint(self):
-        legend, layer_painter = self.obj, self.parent
-
-        kwargs = self.get_kwargs()
-        border = kwargs.pop('border')
-        visible = kwargs.pop('visible')
-        handles, labels = kwargs.pop('handles'), kwargs.pop('labels')
-        print "labels", labels
-        _legend = layer_painter.axes.legend(handles, labels, **kwargs)        
-        _legend.draw_frame(border)
-        _legend.set_visible(visible)
-                
-
-    def get_kwargs(self):
-        legend, layer_painter = self.obj, self.parent
-
-        # visible
-        visible = legend.visible
-                    
-        # labels # TODO
-        label = legend.label
-        if label is not None:
-            pass
-
-        # border
-        border = legend.border
-
-        # position
-        position = legend.position
-        if position == 'at position':
-            position = (legend.x, legend.y)
-
-        # create legend entries from line labels
-        line_cache = layer_painter.axes.lines
-        labels = [l.get_label() for l in line_cache]
-
-        return  {'handles' : line_cache,
-                 'labels' : labels,
-                 'loc' : position,
-                 'border' : border,
-                 'visible' : visible}
+    def redraw(self, force=False):
+        """ redraw, unlike draw, only redisplays the existing canvas. """
+        if self._redraw or force is True:
+            print "REDRAW"
+            self.canvas.draw()
+        self._redraw = False
+        
         
 #------------------------------------------------------------------------------    
 globals.BackendRegistry['matplotlib'] = Backend
--- trunk/Sloppy/src/Sloppy/Plugins/PeakFinder/gui_gtk.py
+++ trunk/Sloppy/src/Sloppy/Plugins/PeakFinder/gui_gtk.py
@@ -4,22 +4,35 @@ import main
 import gtk
 
 from Sloppy.Lib.Props import *
-from Sloppy.Gtk import tools
+from Sloppy.Gtk import toolbox
+from Sloppy.Base import globals, backend
+
 #----------------------------------------------------------------------
 
 # we need to specify a x column and a y column
-
-# Tool,BasedOnLayer
-#
-
-class PeakFinderTool(tools.Tool):
+    
+    
+class PeakFinder(toolbox.Tool):
 
     name = "PeakFinder"
     stock_id = gtk.STOCK_EDIT
 
-    
+
     def __init__(self):
-        tools.Tool.__init__(self)
+        toolbox.Tool.__init__(self)
+        print ">>> INIT PEAKFINDER"
+        
+        # for generic_on_update
+        self.signalmap = {}        
+        self.dependency_chain = ['active_backend',
+                                 'active_layer',
+                                 'active_line']
+        
+        for var in self.dependency_chain:
+            setattr(self, var, None)
+        globals.app.sig_connect('update::active_backend',
+          lambda sender, value: self.generic_on_update('active_backend', sender, value))
+
 
         # info label (displaying the active line)
         label = self.label = gtk.Label()
@@ -38,11 +51,47 @@ class PeakFinderTool(tools.Tool):
         #treeview.connect("cursor-changed", self.on_cursor_changed)
         treeview.show()
         self.add(treeview)
+       
 
-        
+    def generic_on_update(self, var, sender, value):
+        print "GENERIC UPDATE FOR VAR ", var
+        print "%s = %s" % (sender, value)
 
-        
+        old_value = getattr(self, var)
+        if value == old_value:
+            return
+
+        if self.signalmap.has_key(sender):
+            signal = self.signalmap.pop(sender)
+            # propagate None value before disconnecting
+            if value is None and old_value is not None:
+                signal(old_value, None)
+            signal.disconnect()                
+
+        setattr(self, var, value)                
+        if value is not None:
+            # TODO: this already assumes that we have active_backend
+            if not isinstance(value, backend.Backend):
+                obj = self.active_backend.get_painter(value)
+            else:
+                obj = value
+            
+            # Connect to the update mechanism of the next dependency.
+            # If this is already the last dependency, then we call
+            # on_update_object, which then can use the object.
+            try:
+                next_var = self.dependency_chain[self.dependency_chain.index(var)+1]
+            except IndexError:
+                self.on_update_object(sender, value)
+            else:
+                new_signal = obj.sig_connect('update::%s'%next_var,
+                  lambda sender,value: self.generic_on_update(next_var, sender, value))
+                self.signalmap[sender] = new_signal            
+
+    def on_update_object(self, sender, value):
+        print "------------- UPDATE -----------------"
+
 
 
 def gtk_init(app):
-    tools.register_tool(PeakFinderTool, 'PeakFinder')
+    toolbox.register_tool(PeakFinder, 'PeakFinder')
