13743a29e976bf89d57e5191f4a593604f2d893c
niklasv <niklasv@2d33525a-e5fd-0310-a46b-adbed02b8dd7>
2006-12-21 13:21:54 +0000

    Modified example a little.

--- trunk/nvlib/examples/props/cookbook.py
+++ trunk/nvlib/examples/props/cookbook.py
@@ -8,6 +8,8 @@ class SimpleRecipe(HasProperties):
 
     name = vprop(validators.String())
     difficulty = vprop(validators.Integer())
-
+    author = vprop(validators.String())
+    date_entered = vprop(validators.String())
+    comment = vprop(validators.String())
 
 
--- trunk/nvlib/examples/props/simple-usage.py
+++ trunk/nvlib/examples/props/simple-usage.py
@@ -1,15 +1,28 @@
 
 """
 This example illustrates setting and getting values via the Property
-mechanism.
+mechanism. The attributes of a SimpleRecipe are set in a number of
+different ways and are then retrieved.
 """
 
 from cookbook import *
 
 
 def main():
+    print globals()['__doc__']
+    
+    # set prop values during initialization
     r = SimpleRecipe(name=5.0, difficulty=3)
 
+    # set using attribute notation
+    r.author = "Niklas"
+    
+    # or use the 'set' method
+    r.set(date_entered='2006-12-21')
+    r.set('comment', 'very tasty!')
+
+
+    # retrieve all prop values
     for k in r._props.keys():
         v = getattr(r, k)
         print "%12s : %s (%s)" % (k, v, type(v))
@@ -17,6 +30,5 @@ def main():
 
 
 if __name__ == "__main__":
-    print globals()['__doc__']
     main()
     
